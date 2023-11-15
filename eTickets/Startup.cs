fef602bb7b99b00cb678f8a6a0aa3d36363381e6
Home <Home@DESKTOP-M74T0OQ>
2023-11-15 06:36:26 -0800

    Add project files.

---
+++ eTickets/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using eTickets.Data; // Assuming this is your namespace for DbContext
+
+public class Startup
+{
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+
+	public IConfiguration Configuration { get; }
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		// Add services to the container.
+		services.AddControllersWithViews();
+
+		// DbContext configuration
+		var connectionString = Configuration.GetConnectionString("DefaultConnectionString");
+		services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
+		else
+		{
+			app.UseExceptionHandler("/Home/Error");
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.UseAuthorization();
+
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+		});
+
+		AppDbInitializer.Seed(app);
+	}
+}
